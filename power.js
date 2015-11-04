3e69659784bd0780d5b7eceb0561eebc81ef75a2
KeenanHoffman <keenan.m.hoffman@gmail.com>
2015-11-03 23:45:39 -0700

    working without 0 or negative numbers

---
+++ power.js
@@ -0,0 +1,5 @@
+function loopIt() {
+  return arguments;
+}
+
+console.log(loopIt("monday","tuesday","wednesday","thursday","friday"));
---
+++ sum_of_a_range.js
@@ -0,0 +1,21 @@
+function range(start, end, num) {
+      var range_array = [];
+    if (num === undefined) {
+      num = 1;
+    }
+    for(i = start; i <= end; i = i + num) {
+        range_array.push(i);
+        console.log(range_array);
+    }
+    return range_array;
+}
+
+function sum(array) {
+    array_sum = 0;
+    for (i = 0; i < array.length; i++) {
+        var array_sum = array[i] + array_sum;
+        console.log(i,array_sum);
+    }
+    return array_sum;
+}
+console.log(sum(range(1,10,2)));
